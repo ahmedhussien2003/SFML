a7017b777734cb6fb430771aa54a0bc1fbcb3135
ahmed <ahmed2003@gmail.com>
2023-03-05 23:06:59 +0200

    add neew thing

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,7 @@
 using namespace std;
 int main()
 {
-	/*int a, b, c, x;
-	long long b1, b2, b3;
-	cin >> a >> b >> c;*/
-	int x = 1 - 5;
-	cout << abs(x);
-	//b1=pow()
+	cout << "hello my team";
 
 	return 0;
 }
