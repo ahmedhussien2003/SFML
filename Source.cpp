c76e69dd4a1180341dbd1dc9083c009cf8e15c01
Ahmed Hussien Sultan <120306453+ahmedhussien2003@users.noreply.github.com>
2023-03-05 22:54:26 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	/*int a, b, c, x;
+	long long b1, b2, b3;
+	cin >> a >> b >> c;*/
+	int x = 1 - 5;
+	cout << abs(x);
+	//b1=pow()
+
+	return 0;
+}
